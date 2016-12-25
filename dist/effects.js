866ac81784aa77d92d75b1f237e0bf880337f9e0
Sid Hegde <Garfieldfan99@gmail.com>
2016-12-25 11:13:47 -0800

    Create effects.js

---
+++ dist/effects.js
@@ -0,0 +1,10 @@
+    var myVar;
+    
+    function load() {
+        myVar = setTimeout(showPage, 3000);
+    }
+    
+    function showPage() {
+      document.getElementById("loader").style.display = "none";
+      document.getElementById("Content").style.display = "block";
+    }
